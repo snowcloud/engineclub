75e0280579e3a3ac19fc066f1a91929f3e57218c
derek <derek@macbook.local>
2010-02-11 10:01:31 +0000

    project files

---
+++ ecengine/settings.py
@@ -0,0 +1,74 @@
+# Django settings for ecengine project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('your name', 'email@exmaple.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'engineclub',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'Europe/London'
+DATE_FORMAT='%d %B %Y, %H:%M'
+LANGUAGE_CODE = 'en-gb'
+
+SITE_ID = 1
+
+from os import path as os_path
+PROJECT_PATH = os_path.abspath(os_path.split(__file__)[0])
+
+MEDIA_ROOT = os_path.join(PROJECT_PATH, 'media')
+TEMPLATE_DIRS = (
+    os_path.join(PROJECT_PATH, 'sitetemplates')
+)
+
+USE_I18N = True
+MEDIA_URL = ''
+ADMIN_MEDIA_PREFIX = '/admin/media/'
+SECRET_KEY = 'ep1n==cyo=%%p#+aie!ixnuky&tnpwz8_7!i8ot^a()#--0ls3'
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'ecengine.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'apps.aliss',
+    'apps.enginecab',
+    # 'apps.depot',
+    # 'apps.bunker'
+)
+# override any of the above in your own settings_local.py
+try:
+    from settings_local import *
+except ImportError:
+    pass
+
---
+++ ecengine/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    
+    (r'^$', 'apps.aliss.views.index'),
+    (r'^cab/', include('apps.enginecab.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
