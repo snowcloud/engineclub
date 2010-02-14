c46c9d74d9e4b4c94bfb44f8400c1d2875a7fbdc
derek <derek@macbook.config>
2010-02-14 14:22:40 +0000

    started on depot models

--- ecengine/apps/depot/models.py
+++ ecengine/apps/depot/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class TestItem(models.Model):
+
+    idx = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, max_length=80)
+    
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        pass
+        # print 'saving'
+        
+    def __unicode__(self):
+        return '%s: %s' % (self.idx, self.name)
+     
--- ecengine/apps/depot/tests.py
+++ ecengine/apps/depot/tests.py
@@ -7,12 +7,30 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from apps.depot.models import TestItem
+from apps.depot.forms import ItemForm
+
+class ItemTest(TestCase):
+    def test_newitem(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        item = TestItem()
+        item.name = 'fred'
+        item.save()
+        self.failUnlessEqual(item.name, 'fred')
+        
+    def test_form(self):
+        """docstring for test_form"""
+        item = TestItem()
+        item.name = 'fred'
+        form = ItemForm({'name': 'joe'}, instance=item)
+        i = form.save(commit=False)
+        # 
+        print i
+        print form.is_valid()
+        print form
+        
 
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
@@ -20,4 +38,34 @@ Another way to test that 1 + 1 is equal to 2.
 >>> 1 + 1 == 2
 True
 """}
+
+
+# # from django.db import models
+# 
+# # Create your models here.
+# 
+# 
+# from django.core.management import setup_environ
+# 
+# import sys, os
+# sys.path.append('/Users/derek/dev_django')
+# sys.path.append('/Users/derek/dev_django/shared_apps')
+# 
+# from ecengine import settings
+# 
+# setup_environ(settings)
+# 
+# from django.db import models
+# 
+# class TestItem(models.Model):
+# 
+#     name = models.CharField(blank=True, max_length=80)
+#     
+#     # class Meta:
+#     #     abstract = True
+# 
+# # from smgclasses.models import SMGClass
+# print 'blah'
+# 
+
 
