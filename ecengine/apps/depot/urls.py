c3f9b46dded037917563840a6571910490a7bbc9
snowcloud <derek@snowcloud.co.uk>
2010-02-15 01:55:38 +0000

    added url names to depot

--- ecengine/apps/depot/urls.py
+++ ecengine/apps/depot/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     
     url(r'^item/$', 'apps.depot.views.item_index', name='item-list'),
     url(r'^item/add/$', 'apps.depot.views.item_add', name='item-add'),
+    url(r'^item/edit/(?P<object_id>\w+)/$', 'apps.depot.views.item_edit', name='item-edit'),
+    url(r'^item/remove/(?P<object_id>\w+)/$', 'apps.depot.views.item_remove', name='item-remove'),
     url(r'^item/(?P<object_id>\w+)/$', 'apps.depot.views.item_detail', name='item'),
 
 )
--- ecengine/apps/depot/views.py
+++ ecengine/apps/depot/views.py
@@ -1,11 +1,20 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from apps.depot.models import Item
 from apps.depot.forms import *
-from mongoengine.queryset import OperationError
+from mongoengine.queryset import OperationError, MultipleObjectsReturned, DoesNotExist
 
+def get_one_or_404(**kwargs):
+    try:
+       object = Item.objects.get(**kwargs)
+       return object
+    except (MultipleObjectsReturned, DoesNotExist):
+        raise Http404
+    
+    
 def item_index(request):
 
     objects = Item.objects
@@ -15,12 +24,17 @@ def item_index(request):
 
 def item_detail(request, object_id):
 
-    objects = Item.objects(id=object_id)
-    print objects[0].name
-    
+    object = get_one_or_404(id=object_id)
+
     return render_to_response('depot/item_detail.html',
-        RequestContext( request, { 'object': objects[0] }))
+        RequestContext( request, { 'object': object }))
         
+def item_edit(request, object_id):
+    pass
+    
+def item_remove(request, object_id):
+    pass
+    
 def item_add(request):
 
     if request.method == 'POST':
@@ -29,16 +43,10 @@ def item_add(request):
             item = Item(**form.cleaned_data)
             try:
                 item.save()
+                return HttpResponseRedirect(reverse('item', args=[item.id]))
             except OperationError:
                 pass
-            # post = form.save(commit=False)
-            # post.author = request.user
-            # post.slug = slugify(post.title)
-            # post.publish = datetime.datetime.now()
-            # post.ct_group = group
-            # post.save()
             
-            return HttpResponseRedirect('/depot/item/')
     else:
         form = ItemForm(initial={'name': 'fred'})
         
