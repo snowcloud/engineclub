5d88bf924ddf7a754c6f1ebf62555c22e97c4f52
snowcloud <derek@snowcloud.co.uk>
2010-02-16 02:27:09 +0000

    debugging form prob

--- ecengine/apps/depot/forms.py
+++ ecengine/apps/depot/forms.py
@@ -9,15 +9,15 @@ class ItemForm(forms.Form):
     
     url = forms.CharField()
     title = forms.CharField()
-    # description = forms.CharField(required=False)
-    # postcode = forms.CharField(required=False)
-    # area = forms.CharField(required=False)
-    # tags = forms.CharField(required=False)
-    # # last_modified = forms.DateField(required=False)
-    # shelflife = forms.CharField(required=False)
-    # source = forms.CharField(required=False)
-    # status = forms.CharField(required=False)
-    # admin_note = forms.CharField(required=False)
+    description = forms.CharField(required=False)
+    postcode = forms.CharField(required=False)
+    area = forms.CharField(required=False)
+    tags = forms.CharField(required=False)
+    # last_modified = forms.DateField(required=False)
+    shelflife = forms.CharField(required=False)
+    source = forms.CharField(required=False)
+    status = forms.CharField(required=False)
+    admin_note = forms.CharField(required=False)
 
     def clean_url(self):
         data = self.cleaned_data['url']
