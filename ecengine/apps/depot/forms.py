ac6ff1b1c2784d3d6a403d7aeb5d8b80c742094e
snowcloud <derek@snowcloud.co.uk>
2010-03-01 13:37:23 +0000

    pruned ShortItemForm

--- ecengine/apps/depot/forms.py
+++ ecengine/apps/depot/forms.py
@@ -10,14 +10,7 @@ class ShortItemForm(forms.Form):
     url = forms.CharField()
     title = forms.CharField()
     description = forms.CharField(widget=forms.Textarea, required=False)
-    # postcode = forms.CharField(required=False)
-    # area = forms.CharField(required=False)
-    # tags = forms.CharField(required=False)
-    # last_modified = forms.DateField(required=False)
-    # shelflife = forms.CharField(required=False)
     author = forms.CharField(widget=forms.HiddenInput, required=False)
-    # status = forms.CharField(required=False)
-    # admin_note = forms.CharField(required=False)
     instance = None
     
     def clean_url(self):
