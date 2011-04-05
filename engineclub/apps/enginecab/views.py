d374535eacb2ff797069c1e43e594284ce5c0502
Derek Hoy <derek@snowcloud.co.uk>
2011-04-05 14:26:44 +0100

    added is_staff decorator for cab views

--- engineclub/apps/enginecab/views.py
+++ engineclub/apps/enginecab/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -8,13 +8,13 @@ from django.template import RequestContext
 
 from firebox.views import reindex_resources
 
-@login_required
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     
     return render_to_response('enginecab/index.html', RequestContext(request))
 
 
-@login_required
+@user_passes_test(lambda u: u.is_staff)
 def reindex(request):
     
     reindex_resources(settings.MONGO_DB)
