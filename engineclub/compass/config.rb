7a819130b072fc6e84e47d0e04a833704b846264
Derek Hoy <derek@snowcloud.co.uk>
2011-04-07 21:50:11 +0100

    added compass source files.

---
+++ engineclub/compass/config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "../static/css"
+sass_dir = "src"
+images_dir = "images"
+output_style = :compact
+# output_style = :compressed
+# To enable relative image paths using the images_url() function:
+# http_images_path = :relative
+http_images_path = "/images"
