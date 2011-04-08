2fca3ca0432e4ffc37061a233ce978ae059dfee3
Derek Hoy <derek@snowcloud.co.uk>
2011-04-08 10:22:22 +0100

    changed search form label.

--- engineclub/apps/depot/forms.py
+++ engineclub/apps/depot/forms.py
@@ -36,7 +36,7 @@ class DocumentForm(forms.Form):
 class FindResourceForm(forms.Form):
     
     post_code = forms.CharField(help_text='enter a post code or a place name', required=False)
-    tags = forms.CharField(widget=CSVTextInput, help_text='comma separated text (spaces OK)', required=True)
+    tags = forms.CharField(widget=CSVTextInput, label='Search text:', help_text='comma separated text (spaces OK)', required=True)
 
     def __init__(self, *args, **kwargs):
         self.locations = []
