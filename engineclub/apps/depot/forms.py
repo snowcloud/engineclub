69c3eb304b7c036dcb7a55e80b53373bbc746fb1
Derek Hoy <derek@snowcloud.co.uk>
2011-04-15 17:07:48 +0100

    tidied search form

--- engineclub/apps/depot/forms.py
+++ engineclub/apps/depot/forms.py
@@ -35,7 +35,7 @@ class DocumentForm(forms.Form):
 
 class FindResourceForm(forms.Form):
     
-    post_code = forms.CharField(help_text='enter a post code or a place name', required=False)
+    post_code = forms.CharField(label='Location', help_text='enter a post code or a place name', required=False)
     tags = forms.CharField(widget=CSVTextInput, label='Search text:', help_text='comma separated text (spaces OK)', required=True)
     boost_location = forms.CharField(widget=forms.HiddenInput, required=False)
     
