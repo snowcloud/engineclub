148a5905dad232e3bd9c23b7e22b127596d1c068
Derek Hoy <derek@snowcloud.co.uk>
2011-05-10 11:17:25 +0100

    added new cab functions to fix curation probs.

--- engineclub/apps/enginecab/urls.py
+++ engineclub/apps/enginecab/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     
     url(r'^$', 'enginecab.views.index', name='cab'),
-    url(r'^reindex$', 'enginecab.views.reindex', name='cab-reindex'),
-    url(r'^fix-resource-accounts$', 'enginecab.views.fix_resource_accounts', name='cab-fix-resource-accounts'),
-    url(r'^one-off-util$', 'enginecab.views.one_off_util', name='cab-one-off-util'),
+    url(r'^reindex/$', 'enginecab.views.reindex', name='cab-reindex'),
+    url(r'^fix-resource-accounts/$', 'enginecab.views.fix_resource_accounts', name='cab-fix-resource-accounts'),
+    url(r'^one-off-util/$', 'enginecab.views.one_off_util', name='cab-one-off-util'),
+    url(r'^show-curationless-resources/$', 'enginecab.views.show_curationless_resources', name='show-curationless-resources'),
+    url(r'^fix-curationless-resources/$', 'enginecab.views.fix_curationless_resources', name='fix-curationless-resources'),
     
 )
