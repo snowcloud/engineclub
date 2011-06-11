a85e787edfef8617083c0d8ba989b54bd89d620a
Derek Hoy <derek@snowcloud.co.uk>
2011-06-11 01:22:39 +0100

    fixed mongo host to use local ip.

--- engineclub/settings.py
+++ engineclub/settings.py
@@ -84,7 +84,7 @@ TEST_RUNNER = 'depot.tests.MongoDBRunner'
 from mongoengine import connect
 MONGO_DB= 'aliss'
 # connect(MONGO_DB, host='localhost', port=27017)
-connect(MONGO_DB, host='178.79.177.89', port=27017)
+connect(MONGO_DB, host='192.168.176.78', port=27017)
 
 LATLON_SEP= ', '
 
