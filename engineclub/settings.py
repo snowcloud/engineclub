6450f7c2e04993ba66ea315b78b43e1519064758
Derek Hoy <derek@snowcloud.co.uk>
2011-06-11 01:34:44 +0100

    moved mongodb connection to settings_local.

--- engineclub/settings.py
+++ engineclub/settings.py
@@ -73,19 +73,6 @@ DATABASES = {
     }
 }
 
-
-# MongoDBTestRunner creates/drops test db
-# TEST_RUNNER = 'depot.tests.MongoDBTestRunner'
-
-# MongoDBRunner uses current db
-TEST_RUNNER = 'depot.tests.MongoDBRunner'
-
-# repeated connects now fixed in mongoengine
-from mongoengine import connect
-MONGO_DB= 'aliss'
-# connect(MONGO_DB, host='localhost', port=27017)
-connect(MONGO_DB, host='192.168.176.78', port=27017)
-
 LATLON_SEP= ', '
 
 # set/override these in settings_local
