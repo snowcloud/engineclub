a7173af7a202b903bd01b2137c5369e39d39a5a6
Derek Hoy <derek@snowcloud.co.uk>
2011-08-05 18:51:33 +0100

    added depot.templatetags/depot_tags

---
+++ engineclub/apps/depot/templatetags/depot_tags.py
@@ -0,0 +1,24 @@
+from django.template import Library, Node, Variable
+from django.template.defaultfilters import date
+
+register = Library()
+
+@register.simple_tag
+def site_base():
+    return Site.objects.get_current().domain
+
+@register.filter
+def event_date(value, arg=None):
+    """ usage {{ object.calendar_event|event_date }}
+    
+    """
+    def _format_date(d, format=None):
+        # uses django date filter, format defaults to settings.DATE_FORMAT
+        return  date(d, format).replace(', 00:00', '')
+
+    if value.start:
+        s = _format_date(value.start)
+        e = ' - %s' % _format_date(value.end) if value.end else ''
+        return '%s%s' % (s, e)
+    else:
+        return ''
