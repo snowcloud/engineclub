c2baf1aeef6729bbbd13674e416d2918de464341
Dougal Matthews <dougal85@gmail.com>
2011-11-11 16:03:43 +0000

    Made the Pagination controls a little easier to see and added them to the top and bottom.

--- demo/living_well/javascripts/aliss.js
+++ demo/living_well/javascripts/aliss.js
@@ -1,5 +1,4 @@
 
-
 function validate_location(location_name){
 
     $.ajax({
@@ -31,6 +30,35 @@ function aliss_query(defaults, handler){
         url: 'http://aliss.org/api/resources/search/',
         success: handler
     });
+}
+
+function add_pagination_buttons(div_id, defaults, count, events){
+
+    if (defaults.start > 0){
+        $(div_id).append(' <a href="#" class="pagination previous">&lt;- Previous</a>');
+        if (events){
+            $(div_id + " a.previous").click(function(){
+                var previous_params = jQuery.extend({}, defaults);
+                previous_params.start -= previous_params.max;
+                if (previous_params.start < 0){
+                    previous_params.start = 0;
+                }
+                aliss_search(previous_params, div_id, true);
+            });
+        }
+    }
+
+    if (count >= defaults.max){
+        $(div_id).append(' <a href="#" class="pagination next">Next -&gt;</a>');
+        if (events){
+            $(div_id + " a.next").click(function(){
+                var next_params = jQuery.extend({}, defaults);
+                next_params.start += next_params.max;
+                aliss_search(next_params, div_id, true);
+            });
+        }
+    }
+
 }
 
 function aliss_search(data, div_id, paginate){
@@ -67,61 +95,52 @@ function aliss_search(data, div_id, paginate){
         success: function(response){
 
             var count = response.data[0].results.length;
+
             if (count < 1){
 
                 $(div_id).html("<h3>No results found</h3>");
-                return;
+
+            } else {
+
+
+
+                if (response.data[0].location[0]){
+                    var latlng = response.data[0].location[0].split(', ');
+                    google_map.setCenter(new google.maps.LatLng(latlng[0], latlng[1]));
+                }
+
+                var items = [];
+                var markers = [];
+
+                $.each(response.data[0].results, function(index, value){
+                    items.push('<li><a href="' + value.uri + '">' + value.title + '</a><p>' + value.description + '</p></li>');
+                    if (value.locations[0]){
+                        var latlng = value.locations[0].split(', ');
+                        var glatlng = new google.maps.LatLng(latlng[0], latlng[1]);
+                        markers.push(new google.maps.Marker({
+                            map:google_map,
+                            draggable:true,
+                            animation: google.maps.Animation.DROP,
+                            position: glatlng
+                        }));
+                    }
+                });
+
+                $(div_id).html("<h3>Support for " + defaults.query + " near to " + defaults.location + "</h3>");
+
+                // Add pagination buttons to the top and bottom
+                if (paginate){
+                    add_pagination_buttons(div_id, defaults, count);
+                }
+
+                $(div_id).append('\n<ul></ul>');
+
+                $(div_id + ' ul').append(items.join(''));
+
             }
 
-            var latlng = response.data[0].location[0].split(', ');
-
-            google_map.setCenter(new google.maps.LatLng(latlng[0], latlng[1]));
-
-            var items = [];
-            var markers = [];
-
-            $.each(response.data[0].results, function(index, value){
-                items.push('<li><a href="' + value.uri + '">' + value.title + '</a><p>' + value.description + '</p></li>');
-                if (value.locations[0]){
-                    var latlng = value.locations[0].split(', ');
-                    var glatlng = new google.maps.LatLng(latlng[0], latlng[1]);
-                    markers.push(new google.maps.Marker({
-                        map:google_map,
-                        draggable:true,
-                        animation: google.maps.Animation.DROP,
-                        position: glatlng
-                    }));
-                }
-            });
-
-            $(div_id).html("<h3>Support for " + defaults.query + " near to " + defaults.location + "</h3>\n<ul></ul>");
-
-            $(div_id + ' ul').append(items.join(''));
-
             if (paginate){
-
-                if (defaults.start > 0){
-                    $(div_id).append('<a hred="#" class="previous">Previous</a>');
-                    $(div_id + " a.previous").click(function(){
-                        var previous_params = jQuery.extend({}, defaults);
-                        previous_params.start -= previous_params.max;
-                        if (previous_params.start < 0){
-                            previous_params.start = 0;
-                        }
-                        aliss_search(previous_params, div_id, paginate);
-                    });
-                }
-
-                if (count >= defaults.max){
-                    $(div_id).append('<a hred="#" class="next">Next</a>');
-                    $(div_id + " a.next").click(function(){
-                        var next_params = jQuery.extend({}, defaults);
-                        next_params.start += next_params.max;
-                        aliss_search(next_params, div_id, paginate);
-                    });
-                }
-
-
+                add_pagination_buttons(div_id, defaults, count, true);
             }
 
         }
