1435b22810dfee05ab2cb2e17b6a62e4357025f8
Dougal Matthews <dougal85@gmail.com>
2011-12-13 14:00:01 +0000

    Pooled the Redis connection for easier re-use.

---
+++ engineclub/apps/analytics/__init__.py
@@ -0,0 +1,30 @@
+from redis.connection import UnixDomainSocketConnection, Connection
+from redis import ConnectionPool
+
+
+class CacheConnectionPool(object):
+    _connection_pool = None
+
+    def get_connection_pool(self, host='127.0.0.1', port=6379, db=1,
+            password=None, unix_socket_path=None):
+
+        if self._connection_pool is None:
+            connection_class = (
+                unix_socket_path and UnixDomainSocketConnection or Connection
+            )
+            kwargs = {
+                'db': db,
+                'password': password,
+                'connection_class': connection_class,
+            }
+            if unix_socket_path is None:
+                kwargs.update({
+                    'host': host,
+                    'port': port,
+                })
+            else:
+                kwargs['path'] = unix_socket_path
+            self._connection_pool = ConnectionPool(**kwargs)
+        return self._connection_pool
+
+pool = CacheConnectionPool()
