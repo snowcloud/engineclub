2bae6757fb04a7045df7beeeb5f8388080a13762
Dougal Matthews <dougal85@gmail.com>
2012-01-18 16:59:41 +0000

    added helper methods to make it easier to work with different severities.

--- engineclub/apps/notifications/models.py
+++ engineclub/apps/notifications/models.py
@@ -17,17 +17,54 @@ class NotificationType(Document):
 class NotificationQuerySet(QuerySet):
 
     def create_for_accounts(self, accounts, **kwargs):
+        """
+        Create notifications for a list of accounts. This is done so that
+        when a notification if given to a group of people, once one person
+        deals with it, we can strike it off for all the others.
+        """
 
         ng = NotificationGroup.objects.create()
 
         for account in accounts:
             Notification.objects.create(account=account, group=ng, **kwargs)
 
+    def create_for_account(self, account, **kwargs):
+        return self.create_for_accounts([account, ], **kwargs)
+
+    def for_account(self, account):
+        """
+        Wrapped into a simpler helper as we may want to change the behaviour
+        later to retrieve notifications for sub or parent accounts based on
+        the membership.
+        """
+        return Notification.objects(account=account)
+
+    def _user(self, account=None):
+        if account:
+            return Notification.objects(account=account)
+        else:
+            return Notification.objects
+
+    def low(self, user=None):
+        return self._user(user)(severity=SEVERITY_LOW)
+
+    def medium(self, user=None):
+        return self._user(user)(severity=SEVERITY_MEDIUM)
+
+    def high(self, user=None):
+        return self._user(user)(severity=SEVERITY_HIGH)
+
+    def critical(self, user=None):
+        return self._user(user)(severity=SEVERITY_CRITICAL)
+
+
+SEVERITY_LOW, SEVERITY_MEDIUM, SEVERITY_HIGH, SEVERITY_CRITICAL = (0, 1, 2, 3)
 
 SEVERITY_CHOICES = (
-    (0, 'Low'),
-    (1, 'Medium'),
-    (2, 'High'),
+    (SEVERITY_LOW, 'Low'),
+    (SEVERITY_MEDIUM, 'Medium'),
+    (SEVERITY_HIGH, 'High'),
+    (SEVERITY_CRITICAL, 'Critical'),
 )
 
 
@@ -42,6 +79,27 @@ class Notification(Document):
     datetime = DateTimeField(default=datetime.now)
     origin = ReferenceField(Account, required=False)
     message = StringField()
+    opened = BooleanField(default=False)
+    resolved = BooleanField(default=False)
 
     def __unicode__(self):
         return self.message
+
+    def mark_read(self):
+        self.opened = True
+        self.save()
+
+    def resolve(self):
+        """
+        If the notification is in a group, update the full notification
+        group - including this one. Otherwise update only this notification.
+        """
+
+        if not self.group:
+            self.resolved = True
+            self.save()
+            return
+
+        for notification in Notification.objects(group=self.group):
+            notification.resolved = True
+            notification.save()
--- engineclub/apps/notifications/tests.py
+++ engineclub/apps/notifications/tests.py
@@ -4,25 +4,33 @@ from mongoengine.django.tests import MongoTestCase
 class NotificationsTestCase(MongoTestCase):
 
     def setUp(self):
+        """
+        Create two test accounts and a parent account with alice being a
+        member.
+        """
 
-        from engine_groups.models import Account
+        from engine_groups.models import Account, Membership
 
-        self.account1 = Account.objects.create(name="Bob", email="bob@example.com")
-        self.account2 = Account.objects.create(name="Alice", email="alice@example.com")
+        self.bob = Account.objects.create(name="Bob", email="bob@example.com")
+        self.alice = Account.objects.create(name="Alice", email="alice@example.com")
+
+        self.company = Account.objects.create(name="company", email="org@example.com")
+
+        Membership.objects.create(parent_account=self.company, member=self.alice)
 
     def test_types(self):
 
         from Notifications.models import NotificationType
 
-        nt, created = NotificationType.objects.get_or_create(name="expired")
+        nt, _ = NotificationType.objects.get_or_create(name="expired")
 
     def test_create(self):
 
         from notifications.models import Notification, NotificationType
 
-        expired, created = NotificationType.objects.get_or_create(name="expired")
+        expired, _ = NotificationType.objects.get_or_create(name="expired")
 
-        accounts = [self.account1, self.account2]
+        accounts = [self.bob, self.alice]
 
         Notification.objects.create_for_accounts(accounts, type=expired,
             severity=1, message='Curation X has expired'
@@ -32,12 +40,66 @@ class NotificationsTestCase(MongoTestCase):
 
         from notifications.models import Notification, NotificationType
 
-        expired, created = NotificationType.objects.get_or_create(name="expired")
+        expired, _ = NotificationType.objects.get_or_create(name="expired")
 
-        self.assertEqual(0, Notification.objects(account=self.account1).count())
+        self.assertEqual(0, Notification.objects.for_account(self.bob).count())
 
-        Notification.objects.create_for_accounts([self.account1, ], type=expired,
+        Notification.objects.create_for_accounts([self.bob, ], type=expired,
             severity=1, message='Curation X has expired'
         )
 
-        self.assertEqual(1, Notification.objects(account=self.account1).count())
+        self.assertEqual(1, Notification.objects.for_account(self.bob).count())
+
+    def test_get_member_notifications(self):
+
+        from notifications.models import Notification, NotificationType
+
+        expired, _ = NotificationType.objects.get_or_create(name="expired")
+        incorrect, _ = NotificationType.objects.get_or_create(name="incorrect")
+
+        self.assertEqual(0, Notification.objects.for_account(self.alice).count())
+
+        # Create a notification for alice and orgnaisation1
+        Notification.objects.create_for_accounts([self.alice, self.company],
+            type=expired, severity=1, message='Curation X is going to expire')
+
+        # Create a notification for orgnanisation1
+        Notification.objects.create_for_accounts([self.company, ],
+            type=incorrect, severity=1, message='Curation Y is incorrect')
+
+        # Create a notification for alice
+        Notification.objects.create_for_accounts([self.alice, ],
+            type=expired, severity=1, message='Curation Z is going to expire')
+
+        self.assertEqual(2, Notification.objects.for_account(self.alice).count())
+        self.assertEqual(2, Notification.objects.for_account(self.company).count())
+
+    def test_group_notifications(self):
+
+        from notifications.models import Notification, NotificationType
+
+        expired, _ = NotificationType.objects.get_or_create(name="expired")
+
+        Notification.objects.create_for_accounts([self.bob, self.alice],
+            type=expired, severity=3, message='Curation X has expired')
+
+        bob_notification, = Notification.objects.for_account(self.bob)
+        alice_notification, = Notification.objects.for_account(self.alice)
+
+        self.assertEqual(bob_notification.group, alice_notification.group)
+
+    def test_severity(self):
+
+        from notifications.models import Notification, NotificationType
+
+        expired, _ = NotificationType.objects.get_or_create(name="expired")
+
+        for i in range(4):
+            for _ in range(i + 1):
+                Notification.objects.create_for_account(self.alice,
+                    type=expired, severity=i, message='Expiration warning')
+
+        self.assertEqual(Notification.objects.low(self.alice).count(), 1)
+        self.assertEqual(Notification.objects.medium(self.alice).count(), 2)
+        self.assertEqual(Notification.objects.high(self.alice).count(), 3)
+        self.assertEqual(Notification.objects.critical(self.alice).count(), 4)
