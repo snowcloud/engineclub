9065d93bdc5ba7c29ee17b93433062402791feb5
Dougal Matthews <dougal85@gmail.com>
2012-01-25 16:31:10 +0000

    Handle empty increment attempts and added tests the search stats.

--- engineclub/apps/analytics/shortcuts.py
+++ engineclub/apps/analytics/shortcuts.py
@@ -2,12 +2,18 @@ from analytics.models import AccountAnalytics
 
 
 def increment_tag(tag_name, account=None):
+    if not tag_name:
+        return
     AccountAnalytics(account).increment_tag(tag_name)
 
 
 def increment_search(query, account=None):
+    if not query:
+        return
     AccountAnalytics(account).increment_search(query)
 
 
 def increment_location(location, account=None):
+    if not location:
+        return
     AccountAnalytics(account).increment_location(location)
