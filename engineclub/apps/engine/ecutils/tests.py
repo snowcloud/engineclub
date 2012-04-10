8a9df099fc78505b022f5b9c50ad36ed3b36faca
Derek Hoy <derek@snowcloud.co.uk>
2012-04-10 22:23:53 +0100

    first tests for ecutils.

--- engineclub/apps/engine/ecutils/tests.py
+++ engineclub/apps/engine/ecutils/tests.py
@@ -1,7 +1,19 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+ecutils.tests
 """
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_utils_minmax(self):
+
+    	from ecutils.utils import minmax
+
+    	self.assertEqual(2, minmax(-2, 9000, 2))
+    	self.assertEqual(-2, minmax(-2, 9000, -3))
+    	self.assertEqual(9000, minmax(-2, 9000, 9000))
+    	self.assertEqual(9000, minmax(-2, 9000, 9001))
+    	self.assertEqual(90, minmax(80, 9000, None, default=90))
+    	self.assertEqual(9000, minmax(80, 9000, None, default=9001))
+    	self.assertEqual(80, minmax(80, 9000, None, default=-18))
 
