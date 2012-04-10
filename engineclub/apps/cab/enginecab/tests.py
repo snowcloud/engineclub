7eb0561ae2492f442ee2a378362f02be301e9fe3
Derek Hoy <derek@snowcloud.co.uk>
2012-04-10 22:03:33 +0100

    tests & more tests.

--- engineclub/apps/cab/enginecab/tests.py
+++ engineclub/apps/cab/enginecab/tests.py
@@ -1,50 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-depot/tests.py
+enginecab/tests.py
 """
 
-import os
-
 from django.conf import settings
-from django.contrib.auth.models import User
-from mongoengine.connection import _get_db as get_db
 from mongoengine.django.tests import MongoTestCase
-from pysolr import Solr
 
-from resources.models import Resource, Curation
-from accounts.models import Account, Collection
-
-SEP = '**************'
-SOLR_ROWS = 5 # settings.SOLR_ROWS
-
-# def _dump_collections(collection_names=None):
-#     if collection_names is None:
-#         collection_names = [coll for coll in get_db().collection_names() if coll != 'system.indexes']
-#     for coll in collection_names:
-#         os.system('mongoexport -d {0} -c {1} -o output_{1}.json'.format(get_db().name, coll))
-
-# def _load_collections(collection_names=None, drop='--drop'):
-#     if collection_names is None:
-#         collection_names = [coll for coll in get_db().collection_names() if coll != 'system.indexes']
-#     for coll in collection_names:
-#         os.system('mongoimport -d {0} -c {1} --file output_{1}.json {2}'.format(get_db().name, coll, drop))
-
-# def _print_db_info():
-#     """docstring for _print_db_info"""
-#     print SEP
-#     print 'Account: ', Account.objects.count()
-#     print 'Membership: ', Membership.objects.count()
-#     print SEP
-
-# def _make_user(name):
-#     return User.objects.create_user(name, email="%s@example.com" % name, password='password')
-
-# def _make_resource(title, tags, owner):
-#     return  Resource.objects.create(title=title, tags=tags, owner=owner)
-
-        
-class TestBase(MongoTestCase):
-
+class ViewsTestCase(MongoTestCase):
     def setUp(self):
         from accounts.tests import setUpAccounts
         from locations.tests import setUpLocations
@@ -53,83 +15,37 @@ class TestBase(MongoTestCase):
         setUpLocations(self)
         setUpResources(self)
 
-        # # _print_db_info()
-        # self.user_bob = _make_user('bob')
-        # self.user_humph = _make_user('humph')
-        # self.user_jorph = _make_user('jorph')
-        # # self.user_group = _make_user('group')
+        from django.test.client import Client
+        self.client = Client()
 
-        # self.bob = Account.objects.create(name="Bob Hope", email="bob@example.com", local_id=str(self.user_bob.id))
-        # self.humph = Account.objects.create(name="Humph Floogerwhippel", email="humph@example.com", local_id=str(self.user_humph.id))
-        # self.jorph = Account.objects.create(name="Jorph Wheedjilli", email="jorph@example.com", local_id=str(self.user_jorph.id))
+    def test_enginecab(self):
+        from django.core.urlresolvers import reverse
 
-    def tearDown(self):
-        # _print_db_info()
-        pass
-        
+        response = self.client.get(reverse('cab'))
+        self.assertEqual(response.status_code, 302)
 
-# class CollectionsTest(TestBase):
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse('cab'))
+        self.assertEqual(response.status_code, 302)
 
-#     def test_collection(self):
-#         from enginecab.views import reindex_resources
+        self.user_bob.is_staff = True
+        self.user_bob.save()
 
-#         # MAKE A COLLECTION
-#         print self.bob, self.jorph
-#         coll1 = Collection.objects.create(name='Test Collection', owner=self.bob)
-#         coll2 = Collection.objects.create(name='Test Collection 2', owner=self.alice)
-#         self.assertEqual(1, Collection.objects(owner=self.bob).count())
+        response = self.client.get(reverse('cab'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Engine Cab")
 
-#         coll1.add_accounts([self.alice, self.jorph])
-#         coll1.add_accounts([self.alice])
-#         coll1.add_accounts([self.jorph])
-#         self.assertEqual(2, len(coll1.accounts))
+        response = self.client.get(reverse('cab_issues'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Issues")
 
-#         self.assertEqual(1, len(self.alice.collections))
-#         self.assertEqual(self.alice.collections[0].name, 'Test Collection')
+        response = self.client.get(reverse('cab_lists'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Lists")
 
-#         coll2.add_accounts([self.jorph])
-#         self.assertEqual(1, len(coll2.accounts))
+        response = self.client.get(reverse('cab_resources'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Resources")
 
-#         self.assertEqual(2, len(self.jorph.collections))
-#         # self.assertEqual(self.humph.collections[0].name, 'Test Collection')
 
-#         self.assertEqual(9, Resource.objects().count())
-#         self.assertEqual(3, Resource.objects(owner=self.alice).count())
-
-#         # TRY SOME SEARCHES
-
-#         reindex_resources()
-#         conn = Solr(settings.SOLR_URL)
-#         kw = {'rows': SOLR_ROWS, 'fl': '*,score', 'qt': 'resources'}
-        
-#         kwords = 'green'
-#         search_accts = [self.jorph, self.alice]
-#         kw['fq'] = 'accounts:(%s)'% ' OR '.join([str(ac.id) for ac in search_accts])
-#         results = conn.search(kwords, **kw)
-#         self.assertEqual(3, len(results))
-
-#         kwords = 'green'
-#         search_accts = [coll1]
-#         kw['fq'] = 'collections:(%s)'% ' OR '.join([str(ac.id) for ac in search_accts])
-#         results = conn.search(kwords, **kw)
-#         self.assertEqual(3, len(results))
-
-#         kwords = 'red'
-#         search_accts = [coll2]
-#         kw['fq'] = 'collections:(%s)'% ' OR '.join([str(ac.id) for ac in search_accts])
-#         results = conn.search(kwords, **kw)
-#         self.assertEqual(1, len(results))
-
-#         coll2.add_accounts([self.bob])
-#         self.assertEqual(2, len(coll2.accounts))
-        
-#         Resource.reindex_for(self.bob)
-
-#         results = conn.search(kwords, **kw)
-#         self.assertEqual(2, len(results))
-
-#         res9 = Resource.objects.create(title='blah 9', tags=['yellow', 'red'], owner=self.bob)
-#         res9.save(reindex=True)
-#         results = conn.search(kwords, **kw)
-#         self.assertEqual(3, len(results))
 
--- engineclub/apps/cab/youraliss/tests.py
+++ engineclub/apps/cab/youraliss/tests.py
@@ -1,7 +1,42 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+youraliss.tests
 """
+from django.conf import settings
+from mongoengine.django.tests import MongoTestCase
+
+class ViewsTestCase(MongoTestCase):
+    def setUp(self):
+        from accounts.tests import setUpAccounts
+        from locations.tests import setUpLocations
+        from resources.tests import setUpResources
+        setUpAccounts(self)
+        setUpLocations(self)
+        setUpResources(self)
+
+        from django.test.client import Client
+
+        self.client = Client()
+
+    def test_youraliss(self):
+        from django.core.urlresolvers import reverse
+
+        response = self.client.get(reverse('youraliss'))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.login(username='bob', password='password')
+        response = self.client.get(reverse('youraliss'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "bob")
+
+        response = self.client.get(reverse('youraliss_account'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account settings")
+
+        response = self.client.get(reverse('youraliss_curations'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Recently curated resources")
+
+        response = self.client.get(reverse('youraliss_lists'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Lists")
 
