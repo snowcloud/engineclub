8012133e74e628d46203b205ae87022a674dce14
Derek Hoy <derek@snowcloud.co.uk>
2012-04-15 18:41:50 +0100

    added aliss it bookmark to site footer, and aliss_bm.js files back into static/js.

---
+++ engineclub/apps/cab/home/static/js/aliss_bm.js
@@ -0,0 +1,14 @@
+var t;
+try {
+ t= ((window.getSelection && window.getSelection()) ||
+ (document.getSelection && document.getSelection()) ||
+ (document.selection && 
+ document.selection.createRange && 
+ document.selection.createRange().text));
+}
+catch(e){ // access denied on https sites
+ t = "";
+}
+var x = new String(t);
+x = x.replace(/\n/g, "||");
+var w=window.open('http://www.aliss.org/depot/resource/add/?popup=true&title='+encodeURIComponent(document.title)+'&page='+encodeURIComponent(location.href)+'&t='+encodeURIComponent(x),'ALISS','scrollbars=1,status=0,resizable=1,location=0,toolbar=0,width=860,height=680');
