3a9f435f30d760f68a0523f38e25591ad6c7196c
Derek Hoy <derek@snowcloud.co.uk>
2012-04-21 00:39:58 +0100

    fix for curation_edit with mongoengine 0.6.

--- engineclub/apps/engine/resources/views.py
+++ engineclub/apps/engine/resources/views.py
@@ -18,7 +18,7 @@ from pymongo.objectid import ObjectId
 from analytics.shortcuts import (increment_queries, increment_locations,
     increment_resources, increment_resource_crud)
 from locations.models import Location, lookup_postcode
-from resources.models import Resource, Curation, CalendarEvent,  \
+from resources.models import Resource, Curation, add_curation, CalendarEvent,  \
     STATUS_OK, STATUS_BAD, Moderation
     # COLL_STATUS_NEW, COLL_STATUS_LOC_CONF, COLL_STATUS_TAGS_CONF, COLL_STATUS_COMPLETE #location_from_cb_value,
 from resources.forms import FindResourceForm, ShortResourceForm, LocationUpdateForm, EventForm, \
@@ -329,7 +329,7 @@ def curation_add(request, object_id, template_name='depot/curation_edit.html'):
             curation = Curation(**form.cleaned_data)
             curation.owner = user
             curation.item_metadata.update(author=user)
-            resource.add_curation(curation)
+            add_curation(resource, curation)
             # TODO: move this into resource.add_curation
             increment_resource_crud('curation_add', account=user)
             index = len(resource.curations) - 1
@@ -373,6 +373,8 @@ def curation_edit(request, object_id, index, template_name='depot/curation_edit.
             curation.item_metadata.update(author=user)
             curation.save()
             increment_resource_crud('curation_edit', account=user)
+            # reload the resource, don't know why, but needed for mongoengine 0.6
+            resource = Resource.objects.get(id=resource.id)
             resource.save(reindex=True)
             return HttpResponseRedirect(reverse('curation', args=[resource.id, index]))
     else:
