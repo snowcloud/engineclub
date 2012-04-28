623d319f99599ff5afd27f8484d0dff8dc1ca003
Derek Hoy <derek@snowcloud.co.uk>
2012-04-28 01:05:54 +0100

    added accounts to stories and handled curations, accounts and flatpages through one view and templates.

--- engineclub/apps/cab/stories/views.py
+++ engineclub/apps/cab/stories/views.py
@@ -1,24 +1,56 @@
 # Create your views here.
 
 from django.conf import settings
-# from django.contrib.flatpages.models import Flatpage
+from django.contrib.flatpages.models import FlatPage
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from pymongo.objectid import ObjectId
+from pymongo.objectid import InvalidId, ObjectId
 
 from ecutils.utils import get_one_or_404
+from accounts.models import Account
 from resources.models import Resource, Curation
 from resources.search import find_by_place_or_kwords
 
 def stories_list(request, template_name='stories/stories_list.html'):
-    # flatpages = Flatpage.objects.filter(id=1)
-    objects = Curation.objects(tags=settings.STORY_TAG)
+
+    # fetch all story objects: Curations, Accounts, Flatpages
+    # populate list of dictionaries in objects
+
+    # TESTS !!!
+
+    objects = [{
+            'id': obj.id,
+            'type': 'curation',
+            'title': obj.resource.title,
+            'url': reverse('stories_detail', args=[obj.id]),
+            'content': obj.note } for obj in Curation.objects(tags=settings.STORY_TAG)]
+    objects.extend([{
+            'id': obj.id,
+            'type': 'account',
+            'title': obj.name,
+            'url': reverse('stories_detail', args=[obj.id]),
+            'content': obj.description } for obj in Account.objects(tags=settings.STORY_TAG)])
+    objects.extend([{
+            'id': obj.id,
+            'type': 'flatpage',
+            'title': obj.title,
+            'url': reverse('stories_detail', args=[obj.id]),
+            # 'url': obj.url,
+            'content': obj.content } for obj in FlatPage.objects.filter(url__startswith='/story/')])
     template_context = {'objects': objects}
-    # print 'stories:', len(objects)
     return render_to_response(template_name, RequestContext(request, template_context))
 
 def stories_detail(request, object_id, template_name='stories/stories_detail.html'):
 
-    object = get_one_or_404(Curation, id=ObjectId(object_id))
-    template_context = {'object': object}
+    try:
+        object = Curation.objects.get(pk=ObjectId(object_id))
+        obj_type = 'curation'
+    except Curation.DoesNotExist:
+        object = get_one_or_404(Account, id=ObjectId(object_id))
+        obj_type = 'account'
+    except InvalidId:
+        object = FlatPage.objects.get(pk=object_id)
+        obj_type = 'flatpage'
+    template_context = {'object': object, 'obj_type': obj_type}
     return render_to_response(template_name, RequestContext(request, template_context))
