1d20a6a43af59773bcd305ae3059210f4aa32137
Derek Hoy <derek@snowcloud.co.uk>
2012-04-29 17:50:54 +0100

    styling 'your account' tab and formatting youraliss urls to use it.

--- engineclub/apps/cab/youraliss/urls.py
+++ engineclub/apps/cab/youraliss/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^account/$', 'youraliss.views.account', name='youraliss_account'),
     url(r'^curations/$', 'youraliss.views.curations', name='youraliss_curations'),
     url(r'^lists/$', 'youraliss.views.lists', name='youraliss_lists'),
+    url(r'^lists/(?P<object_id>\w+)/$', 'youraliss.views.lists_detail', name='youraliss_lists_detail'),
     (r'^issues/', include('issues.urls')),
     url(r'^(?P<object_id>\w+)/$', 'youraliss.views.profile', name='youraliss'),
 
