51ac94d09536eb029c237d1ab1758d67729d4452
Derek Hoy <derek@snowcloud.co.uk>
2012-05-13 14:41:48 +0100

    added tags management to caboose.

--- engineclub/apps/cab/enginecab/tests.py
+++ engineclub/apps/cab/enginecab/tests.py
@@ -4,10 +4,8 @@ enginecab/tests.py
 """
 
 from django.conf import settings
-from ecutils.tests import MongoTestCase
-from ecutils.tests import MongoTestCase
-from ecutils.tests import MongoTestCase
-from ecutils.tests import MongoTestCase
+from django.core.urlresolvers import reverse
+
 from ecutils.tests import MongoTestCase
 
 class ViewsTestCase(MongoTestCase):
@@ -23,7 +21,6 @@ class ViewsTestCase(MongoTestCase):
         self.client = Client()
 
     def test_enginecab(self):
-        from django.core.urlresolvers import reverse
 
         response = self.client.get(reverse('cab'))
         self.assertEqual(response.status_code, 302)
@@ -50,6 +47,65 @@ class ViewsTestCase(MongoTestCase):
         response = self.client.get(reverse('cab_resources'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Resources")
+
+    def test_tagprocessor(self):
+        from enginecab.models import TagProcessor
+        
+        TESTTAGS = ['blah', 'lrg', '123', 'UPPer', 'KEEPieUppie', '   ', 'please, SPLIT,me;up,   now ']
+        TESTTAG_SPLIT = ['blah', 'lrg', '123', 'UPPer', 'KEEPieUppie', 'please', 'SPLIT', 'me', 'up', 'now']
+        TESTTAG_SPLIT_LOWER = ['blah', 'lrg', '123', 'upper', 'KEEPieUppie', 'please', 'split', 'me', 'up', 'now']
+
+        tp = TagProcessor(TESTTAGS)
+        self.assertEqual(tp.tags, TESTTAGS)
+        self.assertEqual(tp.split(True).tags, TESTTAG_SPLIT)
+        self.assertEqual(tp.lower(True, ['KEEPieUppie']).tags, TESTTAG_SPLIT_LOWER)
+
+        tp.tags = TESTTAGS
+        self.assertEqual(tp.split(True).lower(True, ['KEEPieUppie']).tags, TESTTAG_SPLIT_LOWER)
+
+    def test_tags(self):
+        self.client.login(username='bob', password='password')
+
+        response = self.client.get(reverse('cab_tags'))
+        self.assertEqual(response.status_code, 302)
+
+        self.user_bob.is_staff = True
+        self.user_bob.save()
+
+        response = self.client.get(reverse('cab_tags'))
+        self.assertEqual(response.status_code, 302)
+
+        self.user_bob.is_superuser = True
+        self.user_bob.save()
+
+        response = self.client.get(reverse('cab_tags'))
+        self.assertEqual(response.status_code, 200)
+
+        LOWERTAG = 'green'
+        UPPERTAG = LOWERTAG.upper()
+        self.assertContains(response, LOWERTAG)
+
+        response = self.client.get(reverse('cab_tags_upper', args=[LOWERTAG]))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('cab_tags'))
+        self.assertNotContains(response, '"%s"' % LOWERTAG)
+        self.assertContains(response, '"%s"' % UPPERTAG)
+
+        response = self.client.get(reverse('cab_tags_lower', args=[UPPERTAG]))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('cab_tags'))
+        self.assertNotContains(response, '"%s"' % UPPERTAG)
+        self.assertContains(response, '"%s"' % LOWERTAG)
+
+        response = self.client.get(reverse('cab_tags_remove', args=[LOWERTAG]))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('cab_tags'))
+        self.assertNotContains(response, '"%s"' % UPPERTAG)
+        self.assertNotContains(response, '"%s"' % LOWERTAG)
+
+
+
+        # print response
 
 
 
